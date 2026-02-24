67882cf7fb760aac8c78e64f11c9ee7dcf520266
ChaitanyaKrishna2004 <paruchurichaitanyakrishna6@gmail.com>
2026-02-24 23:23:09 +0530

    setup the redux and created the login

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,22 @@
+import { Provider } from 'react-redux';
 import './App.css'
 import Body from './components/Body'
 import Login from './components/Login'
 import { BrowserRouter, Routes, Route } from "react-router";
+import appStore from './utils/appStore';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   )
 }
--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [Login, setLogin] = useState("");
+    const [Password, setPassword] = useState("");
+
+    const handlelogin = () => {
+        const res = axios({
+            method: 'post',
+            url: '',
+            data: {
+                Login,
+                Password
+            }
+        })
+    };
+
     return (
         <div className="flex justify-center m-20">
             <div className="card card-dash bg-base-300 w-96">
-                <div className="card-body">
-                    <h2 className="card-title justify-center m-5">Login</h2>
-                    <fieldset className="fieldset">
-                        <legend className="fieldset-legend">Login</legend>
-                        <input type="text" placeholder="" className="input input-neutral" />
+                <div className="card-body m-5">
+                    <h2 className="card-title justify-center ">Login</h2>
+                    <fieldset className="fieldset my-2">
+                        <legend className="fieldset-legend">Username or Email </legend>
+                        <input type="text" value={Login} onChange={(e) => setLogin(e.target.value)} placeholder="" className="input input-neutral" />
                     </fieldset>
                     <fieldset className="fieldset">
-                        <legend className="fieldset-legend">password</legend>
-                        <input type="text" placeholder="" className="input input-neutral" />
+                        <legend className="fieldset-legend">Password </legend>
+                        <input type="text" value={Password} onChange={(e) => setPassword(e.target.value)} placeholder="" className="input input-neutral" />
                     </fieldset>
                     <div className="card-actions justify-center">
-                        <button className="btn btn-primary m-5">Login</button>
+                        <button className="btn btn-primary m-2">Login</button>
                     </div>
                 </div>
             </div>
